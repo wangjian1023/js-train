aedc386da10314afdb3c4c7d469012a29ae9eadc
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2017-09-14 15:04:19 +0800

    trainnnnnn

--- slice.js
+++ slice.js
@@ -1,10 +1,19 @@
 /**
- * 
+ * 2017-09-14            wangjian
  */
 
- var arr = ['a', 'b', 'c', 'd', 'e', 'f'];
- console.log(arr.slice(0, 4) );
+var arr = ['a', 'b', 'c', 'd', 'e', 'f'];
 
- console.log(arr.slice(3));
+console.log(`arr.slice(0, 4) => [ ${arr.slice(0, 4)} ]`);
 
- console.log(arr.slice(-1));
+console.log(`arr.slice(3)=>[ ${arr.slice(3)} ]`);
+
+console.log(`arr.slice(-1))=>[ ${arr.slice(-1)} ]`);
+
+console.log(`arr.slice(8)=>[ ${arr.slice(8)} ]`);
+
+console.log(`arr.slice(0,-1)=>[ ${arr.slice(0,-1)} ]`);
+
+console.log(`arr.slice(-1, -3)=>[ ${arr.slice(-1, -3)} ]`);
+
+console.log(`hello ${arr.shift}`);
