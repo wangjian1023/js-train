8512e9e1e5094adb292be6239a078b416b2f2fcd
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2017-09-14 17:01:37 +0800

    数组迭代

--- iterator.js
+++ iterator.js
@@ -1,29 +1,29 @@
 /**
- *  2017-09-14             wangjian
+ *  2017-09-14             Wang Jian
  */
 
 var arr =('1', '2', '3', '4', '5');
 
-arr.forEach(function(ele){
-    console.log(ele * 2);
-})
+console.log(arr.forEach(function(ele){
+   return ele * 2;
+}))
 
 console.log(arr.map(function(ele){
-    console.log( 2 * ele);
+    return 2 * ele;
 }))
 
-console.log(arr.filter(function(ele){
-    console.log(4 >= ele);
+console.log(arr.filter(function(i){
+    return 4 >= i;
 }))
 
-console.log(arr.reduce(function(prev,next) {
-    console.log(prev + next);
+console.log(arr.reduce(function(a,w) {
+    return a + w ;
 }))
 
-console.log(arr.every(function(){
-    console.log(item < 4);
+console.log(arr.every(function(i){
+    return i< 4;
 }))
 
-console.log(arr.some(function(){
-    console.log(item > 3);
+console.log(arr.some(function(i){
+    return i> 3;
 }))
