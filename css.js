3e633da49acff261d99798c830a12169c69958c2
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2017-09-14 15:46:25 +0800

    123

---
+++ css.js
@@ -0,0 +1,15 @@
+/**
+ *  2017-09-14            wangjian
+ */
+
+var arr = ['1', '2', '3', '5', '6', '7'];
+
+console.log(`arr.push('0', '8')=>[ ${arr.push('0', '8')} ]`);
+
+console.log(arr);
+
+console.log(`arr.pop()=>[ ${arr.pop()} ]`);
+
+console.log(`arr.unshift('-1', '-3')=>[ ${arr.unshift('-1', '-3')} ]`);
+
+console.log(`arr.shift()=>[ ${arr.shift()} ]`);
