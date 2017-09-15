a59d1ddbf16b5247e303c88b2baea7d54a906635
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2017-09-15 09:28:57 +0800

    对象属性布尔 函数调用 整数

--- iterator.js
+++ iterator.js
@@ -2,28 +2,28 @@
  *  2017-09-14             Wang Jian
  */
 
-var arr =('1', '2', '3', '4', '5');
+var arr = [1, 2, 3, 4, 5];
 
 arr.forEach(function(ele){
-   console.log(ele * 2);
-})
+   console.log(ele);
+});
 
 console.log(arr.map(function(ele){
     return 2 * ele;
-}))
+}));
 
 console.log(arr.filter(function(i){
-    return 4 >= i;
-}))
+    return i >= 4;
+}));
 
-console.log(arr.reduce(function(a,w) {
-    return a + w ;
-}))
+console.log(arr.reduce(function(a,w){
+    return a - w ;
+}));
 
-console.log(arr.every(function(i){
+console.log(arr.every(function(i){  
     return i< 4;
-}))
+}));
 
 console.log(arr.some(function(i){
     return i> 3;
-}))
+}));
