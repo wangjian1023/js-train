f2db6c6f58f5e2cff1e697a59fd4711417812342
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2017-09-18 16:22:13 +0800

    jiujiu81乘法

---
+++ math.js
@@ -0,0 +1,18 @@
+/**
+ * 2017-09-18     Wang Jian 
+ */
+
+/*var arr = [];
+var a;*/
+console.log(`\t1\t2\t3\t4\t5\t6\t7\t8\t9\t`)
+for(var i = 1; i <= 9; i++ ){
+    var line = '';
+    for(var j = 1; j <=i; j++){
+        /*arr.push(i*j);*/
+        line = line +`${i * j}\t`;
+    }
+    line = `${i}\t` + line
+    console.log(line)
+}
+
+
