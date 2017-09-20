cfde119387e496378c81c2daa24b0e94394a4c11
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2017-09-20 19:36:29 +0800

    animal

---
+++ animal.js
@@ -0,0 +1,81 @@
+/**
+* 
+*/
+
+function animal(){
+    
+};
+animal.prototype = {
+    breath : true,
+    e: 'move',
+    broth: function(){
+        console.log('animal.brith')
+    }   
+};
+
+function ovipara(){
+      
+};
+ovipara.prototype = {
+
+    canfly: function(){
+        console.log('ovipara.canfly')
+
+    }
+}
+
+ovipara.prototype.__proto__ = animal.prototype;
+
+function manmal(){
+
+};
+manmal.prototype = {
+    v : 'viviparity',
+    lactation: function(){
+        console.log(`manmal.latation`)
+    }
+
+}
+manmal.prototype.__proto__ = animal.prototype;
+
+function person(){
+    
+};
+
+person.prototype.__proto__ = manmal.prototype;
+
+function brid(){
+   
+};
+brid.prototype.__proto__ = ovipara.prototype;
+
+function chicken(){
+   
+};
+chicken.prototype.__proto__ = ovipara.prototype;
+
+function dog(){
+   
+};
+dog.prototype.__proto__ = manmal.prototype;
+var o = Object.create(dog, {
+    age: {
+        writable: true,
+        configurable: true,
+        value: '1',
+        enumerable: true
+    },
+    name:{
+        writable: true,
+        configurable: true,
+        value: '123',
+        enumerable: true
+    }
+});
+var a = new person();
+var b = new brid();
+var c = new chicken();
+
+console.log(a.v); 
+a.lactation();
+console.log(o.age)
