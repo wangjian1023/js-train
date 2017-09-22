904995117c38a00aec1cd599e41a4f9a44064e29
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2017-09-22 20:38:48 +0800

    scope

--- scope.js
+++ scope.js
@@ -2,15 +2,18 @@
  *    2017-09-18       Wang Jian
  */
 
+ console.log(`--------if-----------------`)
 if(true){
     var x = 3;
 }
 console.log(`x ->${x}`);
 
+console.log(`---------for-------------`)
 for(var a = 1; a <= 10; a++){
     x = x + 1
 }
 console.log(x,a)
+
 console.log('------------dowhile-----------')
 do{
     var b = 4;
