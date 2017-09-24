3a975c65c0a8786a1dbb240947f72d44c709ee3f
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2017-09-24 13:37:08 +0800

    promise

--- promise.js
+++ promise.js
@@ -1,8 +1,8 @@
 /**
- * 
+ *  2017-09-23      Wang Jian
  */
 
-function asyncFunction() {
+/*function asyncFunction() {
     return new Promise(function (resolve, reject) {
       setTimeout(function () {
         resolve('Async Hello world');
@@ -25,3 +25,31 @@ asyncFunction().then(function (value){
 }).catch(function (error) {
     console.log(`catch -${error}`);
 });    
+*/
+
+function asyncFunction() {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve('Async Hello world');
+      }, 2000);
+    });
+}
+asyncFunction().then(function (value){
+    console.log(`${value}`);
+    return value;
+}).then(function (value){
+    console.log(`${value}`)
+    return {
+        value : `qe`,
+        age : 12
+    }
+}).then(function (s){
+    console.log(`${value.value}`-`${value.age}`);
+    console.log(a);
+}).catch(function (error) {
+    console.log(`catch -${error}`);
+    return 'hello'; 
+}).then(function(q){
+    console.log(`${q}`);
+});   
+ 
